dc45750a00abc1af8811a38279366c1e482e0522
William <111514062+WillFDA@users.noreply.github.com>
2024-10-25 15:21:33 +0200

    Feat: Footer (not responsive yet)

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,4 +1,13 @@
+import Logo from "./logo";
+
 const Footer = () => {
-  return <></>;
+  return (
+    <footer className="bg-black lg:pt-16 lg:pb-7 flex flex-col items-center gap-7">
+      <Logo color="#FFFF" />
+      <h3 className="text-white text-xs lg:text-2xl">
+        © 2020 Kasa. All rights reserved
+      </h3>
+    </footer>
+  );
 };
 export default Footer;
